98281a506ea4181c1a402be5ef558e5857d51584
makennamartin97 <makennamartin97@yahoo.com>
2023-02-01 18:53:05 -0500

    set up routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('testing')
+//creating backend api, entry point
+const express = require('express')
+//allows us to create a dotevnv file with our environment variables 
+const dotenv = require('dotenv').config()
+//port to run server on
+const port = process.env.PORT || 8000
+//initialize express
+const app = express()
+
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Server started on ${port}`))
