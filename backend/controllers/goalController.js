c9cca338241e1090cb2952bc94ba770bc34a99e6
makennamartin97 <makennamartin97@yahoo.com>
2023-02-01 19:22:28 -0500

    async handler added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,33 @@
+const asyncHandler = require('express-async-handler')
+
+
 // desc: Get goals
 // route: GET /api/goals
 // access: private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 // desc: Set goal
 // route: POST /api/goals
 // access: private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'Set goal'})
-}
+})
 
 // desc: Update goal
 // route: PUT /api/goals/:id
 // access: private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+})
 
 // desc: Delete goal
 // route: DELETE /api/goals/:id
 // access: private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
